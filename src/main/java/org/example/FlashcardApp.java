38c27fbd98f83355ed808b02d801a2047ee6b84f
Nolan Geiger <nolan.geiger@gmail.com>
2023-05-10 12:47:21 -0400

    Second commit.

--- src/main/java/org/example/FlashcardApp.java
+++ src/main/java/org/example/FlashcardApp.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class FlashcardApp {
     public static void main(String[] args) {
-        System.out.println("Hello, dad!");
+        System.out.println("New things said, dad!");
     }
 }
